a8f3dc8e2ce5984c77c80aaa23f9b6e0a1250600
akiueno <akiueno@users.noreply.github.com>
2019-04-02 14:53:42 +0900

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'surveys#start'
-  get 'survey/:condition_id/:question_id' => 'qusestion#content'
+  root 'questions#start'
+  get 'survey/:condition_id/:question_id' => 'qusestions#content'
   #get 'surveys/:question_number' => 'surveys#question'
 end
