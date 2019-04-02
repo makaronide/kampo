d8b63346bb4eecf6c56429c6b190a46f3ef45df7
akiueno <akiueno@users.noreply.github.com>
2019-04-02 15:05:24 +0900

    ルートのhtml等を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'questions#start'
+  root 'home#top'
+  get 'survey/condition' => 'questions#condition'
   get 'survey/:condition_id/:question_id' => 'qusestions#content'
   #get 'surveys/:question_number' => 'surveys#question'
 end
